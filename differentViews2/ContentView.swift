2c75c2978cedad39b5e1f2960fbd7688116cd060
mabus <m.buscaroli@outlook.com>
2021-06-10 14:24:21 +0100

    Initial Commit

---
+++ differentViews2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  differentViews2
+//
+//  Created by Matteo on 10/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ differentViews2/differentViews2App.swift
@@ -0,0 +1,17 @@
+//
+//  differentViews2App.swift
+//  differentViews2
+//
+//  Created by Matteo on 10/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct differentViews2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
